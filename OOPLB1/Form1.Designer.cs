b87651fc6b1d8d27a7944fa3d99482f54d0e13b6
Chancellor-art <66575368+Chancellor-art@users.noreply.github.com>
2021-07-15 00:23:05 +0300

    Add project files.

---
+++ OOPLB1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace OOPLB1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(65, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\"Якшо працює, не трогай\"";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(335, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " - Конфуций";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Історія №1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::OOPLB1.Properties.Resources.alena_aenami_lost_dream_hd;
+            this.button2.Location = new System.Drawing.Point(204, 163);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 42);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Skyrim";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(204, 211);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "c#";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(204, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 42);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Naruto";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(204, 307);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(114, 42);
+            this.button5.TabIndex = 6;
+            this.button5.Text = ":)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(123, 396);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(269, 42);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Завершення роботи";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "by Ledediuk Vadim KN-20 ^-^";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
